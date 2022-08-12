c5aed22cae342dfda1884f01e5bb0b6770cb49af
dermul.admin <bart.dermul@mil.be>
2022-08-12 13:02:36 +0200

    added statistics

--- HornetTracker/__init__.py
+++ HornetTracker/__init__.py
@@ -2,6 +2,7 @@ import json
 
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_statistics import Statistics
 
 import datetime
 
@@ -21,9 +22,33 @@ app.static_folder = static
 
 db = SQLAlchemy(app)
 
+
+##### STATISTICS COLLECTOR
+class Request(db.Model):
+    __tablename__ = "request"
+
+    index = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    response_time = db.Column(db.Float)
+    date = db.Column(db.DateTime)
+    method = db.Column(db.String)
+    size = db.Column(db.Integer)
+    status_code = db.Column(db.Integer)
+    path = db.Column(db.String)
+    user_agent = db.Column(db.String)
+    remote_address = db.Column(db.String)
+    exception = db.Column(db.String)
+    referrer = db.Column(db.String)
+    browser = db.Column(db.String)
+    platform = db.Column(db.String)
+    mimetype = db.Column(db.String)
+
+
+statistics = Statistics(app, db, Request)
+##########################
+
+
 with app.app_context():
     db.create_all()
-
 
 from HornetTracker.hornets.models.hornet import Hornet
 from HornetTracker.map.models.map import Map
@@ -38,7 +63,6 @@ app.register_blueprint(map_blueprint)
 @app.route('/', methods=["GET", "POST"])
 def index():
     return render_template("index.html")
-
 
 # @app.route('/.well-known/host-meta', methods=["GET"])
 # def well_known_host_meta():
